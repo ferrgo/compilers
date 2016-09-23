a41985b7c429fbcadc0aca2cbfbb7801215b9bb3
otoalves <otopalves@gmail.com>
2016-09-23 19:08:58 -0300

    Testing if github integration is working fine. Just added some terminals to the grammar symbols

--- BasicCode/src/parser/GrammarSymbols.java
+++ BasicCode/src/parser/GrammarSymbols.java
@@ -9,10 +9,31 @@ package parser;
  * @email gustavohpcarvalho@ecomp.poli.br
  */
 public class GrammarSymbols {
+	//TODO Mudar a implementação pra um enum? Fica mais elegante galere 8D
 
 	// Language terminals (starts from 0)
 	public static final int ID = 0;
+
+	public static final int LETTER = 1;
+
+	public static final int DIGIT = 2;
+
+	public static final int LIT_INTEGER = 3;
+
+	public static final int LIT_LOGICAL = 4;
+
+	public static final int LITERAL = 5;
+
+	public static final int TYPE = 6;
+
+	public static final int OP_COMP =
+
+	public static final int
+
+	public static final int
+
+
 	//TODO
 	public static final int EOT = 1000;
-	
+
 }
