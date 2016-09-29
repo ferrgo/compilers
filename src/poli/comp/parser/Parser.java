83c08fa037e796bc340e6bcbbe8c5b0c6a4f22b1
otoalves <otopalves@gmail.com>
2016-09-28 22:23:37 -0300

    Written a part of the parser

--- src/poli/comp/parser/Parser.java
+++ src/poli/comp/parser/Parser.java
@@ -73,6 +73,8 @@ public class Parser {
 		//TODO do we have to do anything else here?
 	}
 
+
+	//Parses the rule PROG ::= (DECLARATION)* (FUNCTION_DECL | SUBPROGRAM_DECL)*  PROG_MAIN EOT
 	public ASTProgram parseProgram(){
 
 		List<ASTSubprogramDeclaration> l_sd = new ArrayList<ASTSubprogramDeclaration>(); // ( ͡◉ ͜ʖ ͡◉) ~trippy
@@ -92,7 +94,7 @@ public class Parser {
 				l_sd.add(parseSubprogramDeclaration());
 			}
 			//parsing function declarations
-			else if (currentToken.getKind == FUNCTION){
+			else if (currentToken.getKind() == FUNCTION){
 				l_fd.add(parseFunctionDeclaration());
 			}
 
@@ -104,11 +106,83 @@ public class Parser {
 		return rv; //return an ASTPROG
 	}
 
-	public void parseMainProgram(){
+
+	//Parses the rule PROG_MAIN ::= PROGRAM ID (STATEMENT)* END PROGRAM
+	public ASTMainProgram parseMainProgram(){
+
+		ASTIdentifier id;
+		List<ASTStatement> l_s = new ArrayList<ASTStatement>();
+
+		//Parses PROGRAM ID
+		accept(PROGRAM);
+		id=parseIdentifier();
+
+		//Parses each statement
+		while(currentToken.getKind() != END){ // will this work?
+			l_s.add(parseStatement());
+		}
+
+		//Parses END PROGRAM
+		accept(END);
+		accept(PROGRAM);
+
+		ASTMainProgram rv = new ASTMainProgram(id,l_s);
+		return rv;
+	}
+
+	public ASTStatement parseStatement(){
+		ASTStatement rv;
+
+		//Parsing variable declarations
+		if(currentToken.getKind()==TYPE){
+			rv = parseDeclaration();
+		}
+		//Parsing assignments and function calls
+		else if(currentToken.getKind()==ID){
+
+			//Saving the id, then checking what kind of statement this is and treating it properly.
+			//TODO should we refactor the grammar so that
+			// we dont have to do this gambiarra?
+			String idText = currentToken.getSpelling();
+			acceptIt();
+			if(currentToken.getKind()==EQUALS){
+				parseAssignment(idText);
+			}else if(currentToken.getKind()==LP){
+				parseFunctionCall(idText);
+			}
+
+		}
+		//Parsing loop control statements
+		else if(currentToken.getKind()==EXIT ||currentToken.getKind()==CONTINUE){
+			parseLoopControlStatement();
+		}
+		//Parsing loops
+		else if(currentToken.getKind()==DO){
+			parseLoop();
+		}
+		//Parsing if statements
+		else if(currentToken.getKind()==IF){
+			parseIfStatement();
+		}
+		//Parsing return statements
+		else if(currentToken.getKind()==RETURN){
+			parseReturnStatement();
+		}
 
 	}
 
-	// accept(TYPE);
-	// accept(DOUBLECOLON);
-	// parseDeclaredVariables();
+	public parseAssignment(String varName){
+
+	}
+
+	public parseFunctionCall(String functionName){
+
+	}
+
+	public ASTDeclaration parseDeclaration(){
+
+		// accept(TYPE);
+		// accept(DOUBLECOLON);
+		// parseDeclaredVariables();
+	}
 }
