61a43e83967b1bf238b265b798aa10a8c9ebf4bc
hgferr <hfg@cesar.org.br>
2016-09-30 18:30:10 -0300

    parse  evolving and more AST

---
+++ src/poli/comp/util/AST/ASTMainProgram.java
@@ -0,0 +1,18 @@
+package poli.comp.util.AST;
+
+import java.util.List;
+
+/**
+ * Created by hgferr on 30/09/16.
+ */
+public class ASTMainProgram {
+
+
+    private final ASTIdentifier id;
+    private final List<ASTStatement> statements;
+
+    public ASTMainProgram(ASTIdentifier id, List<ASTStatement> l_s) {
+        this.id = id;
+        this.statements = l_s;
+    }
+}
